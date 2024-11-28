11e49abb28f68ef1b61ee1ae0fa3995d1db516fc
GFrasson <bielfc27@gmail.com>
2024-11-27 23:31:57 -0300

    Project image contain

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -4,17 +4,12 @@ import { Children, ReactNode } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 import 'swiper/css'
-import 'swiper/css/effect-creative'
+import 'swiper/css/effect-fade'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import styles from './styles.module.css'
 
-import {
-  EffectCreative,
-  Keyboard,
-  Navigation,
-  Pagination,
-} from 'swiper/modules'
+import { EffectFade, Keyboard, Navigation, Pagination } from 'swiper/modules'
 
 interface CarouselProps {
   children: ReactNode
@@ -24,17 +19,11 @@ export function Carousel({ children }: CarouselProps) {
   return (
     <Swiper
       grabCursor={true}
-      effect={'creative'}
-      creativeEffect={{
-        prev: {
-          shadow: true,
-          translate: [0, 0, -400],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
+      effect={'fade'}
+      fadeEffect={{
+        crossFade: true,
       }}
-      modules={[EffectCreative, Navigation, Pagination, Keyboard]}
+      modules={[EffectFade, Navigation, Pagination, Keyboard]}
       className={styles.swiper}
       keyboard={{
         enabled: true,
