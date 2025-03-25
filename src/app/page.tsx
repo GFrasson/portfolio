31dcc656afc7d34958deaf6267a82e5d214e92dc
GFrasson <bielfc27@gmail.com>
2025-03-25 20:49:20 -0300

    Home

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading, Section } from '@radix-ui/themes'
+
+export default function Home() {
+    return (
+        <Section size="4">
+            <Container>
+                <Heading>Portfólio</Heading>
+            </Container>
+        </Section>
+    )
+}
